f5308607f9536689b54fdfaf0d28e7dd04b218f7
Muojama1 <sandramuojama@gmail.com>
2023-02-23 14:06:00 +0100

    changes

---
+++ index.js
@@ -0,0 +1,10 @@
+tudentSubjects='artsSubjects';
+if (studentSubjects='scienceSubjects') {
+	    console.log('Physics, Chemistry, Biology, Technical Drawing');
+} else if (studentSubjects='socialScienceSubjects') {
+	    console.log('Accounting, Commerce, Marketing, Geography');
+} else if (studentSubjects='artsSubjects') {
+	    console.log('Government, Economics, Literature, History');
+} else {
+	    console.log('English, Mathematics')
+}
